5c7fc733869ffd0a782813dc7148e94300da5675
Lukasz Kopyrski <lukasz.kopyrski@wp.pl>
2020-07-06 13:59:55 +0200

    Final version

---
+++ binary.js
@@ -0,0 +1,178 @@
+let now,h,hbf,hbs,m,mbf,mbs,s,sbf,sbs,i=0,mode;
+let time=()=>{
+    now=new Date();
+    h=now.getHours();
+    hbf=Math.floor(h/10);
+    hbs=Math.floor(h%10);
+    hbf=hbf.toString(2);
+    hbs=hbs.toString(2);
+    m=now.getMinutes();
+    mbf=Math.floor(m/10);
+    mbs=Math.floor(m%10);
+    mbf=mbf.toString(2);
+    mbs=mbs.toString(2);
+    s=now.getSeconds();
+    sbf=Math.floor(s/10);
+    sbs=Math.floor(s%10);
+    sbf=sbf.toString(2);
+    sbs=sbs.toString(2);
+    
+     switch(hbf){
+         case "0":
+             document.querySelector('.hour__first').innerHTML='<img src="off.png"></img><img src="off.png"></img><img src="off.png"></img>';
+        break;
+        case "1":
+            document.querySelector('.hour__first').innerHTML='<img src="on.png"></img><img src="off.png"></img><img src="off.png"></img>';
+        break;
+        case "10":
+            document.querySelector('.hour__first').innerHTML="<img src='off.png'></img><img src='on.png'></img><img src='off.png'></img>";
+        break;
+     }
+    
+     switch(hbs){
+        case "0":
+            document.querySelector('.hour__second').innerHTML='<img src="off.png"></img><img src="off.png"></img><img src="off.png"></img><img src="off.png"></img>';
+        break;
+        case "1":
+            document.querySelector('.hour__second').innerHTML='<img src="off.png"></img><img src="off.png"></img><img src="off.png"></img><img src="on.png"></img>';
+        break;
+        case "10":
+            document.querySelector('.hour__second').innerHTML='<img src="off.png"></img><img src="off.png"></img><img src="on.png"></img><img src="off.png"></img>';
+        break;
+        case "11":
+            document.querySelector('.hour__second').innerHTML='<img src="off.png"></img><img src="off.png"></img><img src="on.png"></img><img src="on.png"></img>';
+        break;
+        case "100":
+            document.querySelector('.hour__second').innerHTML='<img src="off.png"></img><img src="on.png"></img><img src="off.png"></img><img src="off.png"></img>';
+        break;
+        case "101":
+            document.querySelector('.hour__second').innerHTML='<img src="off.png"></img><img src="on.png"></img><img src="off.png"></img><img src="on.png"></img>';
+        break;
+        case "110":
+            document.querySelector('.hour__second').innerHTML='<img src="on.png"></img><img src="on.png"></img><img src="off.png"></img><img src="off.png"></img>';
+        case "111":
+            document.querySelector('.hour__second').innerHTML='<img src="off.png"></img><img src="on.png"></img><img src="on.png"></img><img src="on.png"></img>';
+        break;
+        case "1000":
+            document.querySelector('.hour__second').innerHTML='<img src="on.png"></img><img src="off.png"></img><img src="off.png"></img><img src="off.png"></img>';   
+        break;
+        case "1001":
+            document.querySelector('.hour__second').innerHTML='<img src="on.png"></img><img src="off.png"></img><img src="off.png"></img><img src="on.png"></img>';
+        break;
+     }
+
+     switch(mbf){
+        case "0":
+            document.querySelector('.minute__first').innerHTML='<img src="off.png"></img><img src="off.png"></img><img src="off.png"></img>';
+        break;
+        case "1":
+            document.querySelector('.minute__first').innerHTML='<img src="on.png"></img><img src="off.png"></img><img src="off.png"></img>';
+        break;
+        case "10":
+            document.querySelector('.minute__first').innerHTML='<img src="off.png"></img><img src="on.png"></img><img src="off.png"></img>';
+        break;
+        case "11":
+            document.querySelector('.minute__first').innerHTML='<img src="on.png"></img><img src="on.png"></img><img src="off.png"></img>';
+        break;
+        case "100":
+            document.querySelector('.minute__first').innerHTML='<img src="off.png"></img><img src="off.png"></img><img src="on.png"></img>';
+        break;
+        case "101":
+            document.querySelector('.minute__first').innerHTML='<img src="on.png"></img><img src="off.png"></img><img src="on.png"></img>';
+        break;
+      
+    }
+   
+    switch(mbs){
+       case "0":
+           document.querySelector('.minute__second').innerHTML='<img src="off.png"></img><img src="off.png"></img><img src="off.png"></img><img src="off.png"></img>';
+       break;
+       case "1":
+           document.querySelector('.minute__second').innerHTML='<img src="off.png"></img><img src="off.png"></img><img src="off.png"></img><img src="on.png"></img>';
+       break;
+       case "10":
+           document.querySelector('.minute__second').innerHTML='<img src="off.png"></img><img src="off.png"></img><img src="on.png"></img><img src="off.png"></img>';
+       break;
+       case "11":
+           document.querySelector('.minute__second').innerHTML='<img src="off.png"></img><img src="off.png"></img><img src="on.png"></img><img src="on.png"></img>';
+       break;
+       case "100":
+           document.querySelector('.minute__second').innerHTML='<img src="off.png"></img><img src="on.png"></img><img src="off.png"></img><img src="off.png"></img>';
+       break;
+       case "101":
+           document.querySelector('.minute__second').innerHTML='<img src="off.png"></img><img src="on.png"></img><img src="off.png"></img><img src="on.png"></img>';
+       break;
+       case "110":
+           document.querySelector('.minute__second').innerHTML='<img src="on.png"></img><img src="on.png"></img><img src="off.png"></img><img src="off.png"></img>';
+        break;   
+       case "111":
+           document.querySelector('.minute__second').innerHTML='<img src="off.png"></img><img src="on.png"></img><img src="on.png"></img><img src="on.png"></img>';
+       break;
+       case "1000":
+           document.querySelector('.minute__second').innerHTML='<img src="on.png"></img><img src="off.png"></img><img src="off.png"></img><img src="off.png"></img>';   
+       break;
+       case "1001":
+           document.querySelector('.minute__second').innerHTML='<img src="on.png"></img><img src="off.png"></img><img src="off.png"></img><img src="on.png"></img>';
+       break;
+    }
+
+    switch(sbf){
+        case "0":
+           document.querySelector('.second__first').innerHTML='<img src="off.png"></img><img src="off.png"></img><img src="off.png"></img>';
+       break;
+       case "1":
+           document.querySelector('.second__first').innerHTML='<img src="on.png"></img><img src="off.png"></img><img src="off.png"></img>';
+       break;
+       case "10":
+           document.querySelector('.second__first').innerHTML='<img src="off.png"></img><img src="on.png"></img><img src="off.png"></img>';
+       break;
+       case "11":
+           document.querySelector('.second__first').innerHTML='<img src="on.png"></img><img src="on.png"></img><img src="off.png"></img>';
+       break;
+       case "100":
+           document.querySelector('.second__first').innerHTML='<img src="off.png"></img><img src="off.png"></img><img src="on.png"></img>';
+       break;
+       case "101":
+           document.querySelector('.second__first').innerHTML='<img src="on.png"></img><img src="off.png"></img><img src="on.png"></img>';
+       break;
+    }
+   
+    switch(sbs){
+       case "0":
+           document.querySelector('.second__second').innerHTML='<img src="off.png"></img><img src="off.png"></img><img src="off.png"></img><img src="off.png"></img>';
+       break;
+       case "1":
+           document.querySelector('.second__second').innerHTML='<img src="off.png"></img><img src="off.png"></img><img src="off.png"></img><img src="on.png"></img>';
+       break;
+       case "10":
+           document.querySelector('.second__second').innerHTML='<img src="off.png"></img><img src="off.png"></img><img src="on.png"></img><img src="off.png"></img>';
+       break;
+       case "11":
+           document.querySelector('.second__second').innerHTML='<img src="off.png"></img><img src="off.png"></img><img src="on.png"></img><img src="on.png"></img>';
+       break;
+       case "100":
+           document.querySelector('.second__second').innerHTML='<img src="off.png"></img><img src="on.png"></img><img src="off.png"></img><img src="off.png"></img>';
+       break;
+       case "101":
+           document.querySelector('.second__second').innerHTML='<img src="off.png"></img><img src="on.png"></img><img src="off.png"></img><img src="on.png"></img>';
+       break;
+       case "110":
+           document.querySelector('.second__second').innerHTML='<img src="off.png"></img><img src="on.png"></img><img src="on.png"></img><img src="off.png"></img>';
+        break;   
+       case "111":
+           document.querySelector('.second__second').innerHTML='<img src="off.png"></img><img src="on.png"></img><img src="on.png"></img><img src="on.png"></img>';
+       break;
+       case "1000":
+           document.querySelector('.second__second').innerHTML='<img src="on.png"></img><img src="off.png"></img><img src="off.png"></img><img src="off.png"></img>';   
+       break;
+       case "1001":
+           document.querySelector('.second__second').innerHTML='<img src="on.png"></img><img src="off.png"></img><img src="off.png"></img><img src="on.png"></img>';
+       break;
+    }
+    
+    document.querySelector('.digital').innerHTML=`${h}:${m}:${s}`
+
+    
+}
+
+setInterval(time,1000);
---
+++ readme.md
@@ -0,0 +1,6 @@
+Binary clock used features:
+- HTML5
+- JavaScript es6
+-CSS 3
+-Grid
+-Flexbox
